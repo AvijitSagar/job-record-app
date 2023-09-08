1ccd954c0b9bfb6bd0277745dfe477a08dca321e
avijit biswas sagar <saggarr.work@gmail.com>
2023-09-08 22:05:11 +0600

    login route changed from /dashboard to /home in app/providers/routeserviceprovider.php

--- resources/views/job-record/master.blade.php
+++ resources/views/job-record/master.blade.php
@@ -17,50 +17,38 @@
 
 <body>
     {{-- header start --}}
-
-    <head>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">JOB</a>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+            <div class="container">
+                <a class="navbar-brand" href=""><b><i>NOTE APP</i></b></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link {{ Request::routeIs('') ? 'active' : '' }}" aria-current="page"
+                                href="">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
+                            <a class="nav-link {{ Request::routeIs('') ? 'active' : '' }}"
+                                aria-current="page" href="">Add Note</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                            <a class="nav-link {{ Request::routeIs('note.manage') ? 'active' : '' }}"
+                                href="">Your Notes</a>
                         </li>
                     </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    <form class="d-flex" action="{{ route('logout') }}" method="POST">
+                        @csrf
+                        <button class="btn btn-outline-danger" type="submit">Logout</button>
                     </form>
                 </div>
             </div>
         </nav>
-    </head>
+    </header>
     {{-- header end  --}}
 
 
