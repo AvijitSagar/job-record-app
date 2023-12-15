758d25c882fcb70b6c86e75f8f85484ded41cee3
avijit biswas sagar <saggarr.work@gmail.com>
2023-12-15 16:46:24 +0600

    min url changed into register page and laravel logo removed from the login and registration page

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.register');
 });
 
 Route::get('/home', [JobRecordController::class, 'create'])->middleware(['auth', 'verified'])->name('home');
